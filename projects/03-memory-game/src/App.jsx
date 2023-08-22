727a36b5679703157f80b72fc88cb9a1d63a5e07
martin <martinvillablanca.castro@gmail.com>
2023-08-21 21:22:00 -0400

    update memory game

--- projects/03-memory-game/src/App.jsx
+++ projects/03-memory-game/src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import confetti from 'canvas-confetti'
 import './App.css'
 
 import { CARDS } from './constants/constants'
@@ -13,7 +14,11 @@ function App() {
   const [turn, setTurn] = useState(0)
 
   useEffect(() => {
+    const prevCards = cards
+    if (checkWin(prevCards)) return 
+  }, [cards])
 
+  useEffect(() => {
     const cleanValues = () => {
       setChoiceOne(null)
       setChoiceTwo(null)
@@ -21,8 +26,11 @@ function App() {
       setTurn(newTurn)
     }
 
+    // const prevCards = cards
+    // if (prevCards.every(element => element.match === true)) return confetti()
+
     if (choiceOne && choiceTwo) {
-      if (choiceOne.emoji === choiceTwo.emoji) {
+      if (choiceOne.emoji === choiceTwo.emoji && choiceOne.id !== choiceTwo.id) {
         setCards(prevCards => {
           return prevCards.map( card => {
             return card.emoji === choiceOne.emoji ? {...card, match: true} : card
@@ -32,7 +40,12 @@ function App() {
       }
       cleanValues()
     }
-  }, [choiceOne, choiceTwo, turn])
+  }, [choiceOne, choiceTwo, turn, cards])
+
+  const checkWin = (element) => {
+    if (element.every(item => item.match === true)) return confetti()
+  }
+
 
   // Takes the array of emojis, duplicates it and then shuffle it
   const randomizeCards = () => {
@@ -42,6 +55,7 @@ function App() {
 
     setCards(shuffledCards)
     setTurn(0)
+    console.log(shuffledCards)
   } 
 
   const handleChoice = (card) => {
@@ -59,7 +73,7 @@ function App() {
             key={card.id} 
             card={card} 
             handleChoice={handleChoice} 
-            isSelected={ card === choiceOne || card === choiceTwo || card.matched } />
+            isSelected={ card === choiceOne || card === choiceTwo|| card.match } />
         ))}
       </div>
     </div>
--- projects/03-memory-game/src/components/Card.jsx
+++ projects/03-memory-game/src/components/Card.jsx
@@ -8,8 +8,8 @@ export const Card = ({card, handleChoice, isSelected}) => {
 
     return (
         <div className='card'>
-            <div className={ isSelected ? 'flipped' : '' }>
-                <div className='front' alt='card-front' onClick={handleClick}>{card.emoji}</div>
+            <div className={ isSelected ? 'flipped' : 'unflipped'} onClick={handleClick}>
+                <div className='front' alt='card front' >{card.emoji}</div>
                 <div className='back' alt='card back'></div>
             </div>
         </div>
